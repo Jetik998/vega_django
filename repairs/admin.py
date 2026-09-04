ef75dca5a68290d7a2a73e5e4113d246866e35b0
jet <l-lellboy@yandex.ru>
2026-09-04 20:58:18 +0400

    feat: register repair models in django admin

--- repairs/admin.py
+++ repairs/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Repair, StatusHistory
 
-# Register your models here.
+class StatusHistoryInline(admin.TabularInline):
+    model = StatusHistory
+    extra = 0
+    readonly_fields = ('changed_at', 'status', 'completed_works')
+    can_delete = False
+
+@admin.register(Repair)
+class RepairAdmin(admin.ModelAdmin):
+    list_display = ('id', 'client_name', 'client_phone', 'device_model', 'status', 'cost', 'accepted_at')
+    list_filter = ('status', 'accepted_at')
+    search_fields = ('id', 'client_name', 'client_phone', 'device_model')
+    inlines = [StatusHistoryInline]
+
+@admin.register(StatusHistory)
+class StatusHistoryAdmin(admin.ModelAdmin):
+    list_display = ('repair', 'status', 'changed_at', 'completed_works')
+    list_filter = ('status', 'changed_at')
